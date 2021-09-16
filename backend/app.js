7d27ca487125156571cf74c8c71d6ebb10945034
Salima <salima.almeras@laposte.net>
2021-09-16 14:25:34 +0100

    A

--- backend/app.js
+++ backend/app.js
@@ -1,31 +1,54 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+ 
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
+app.use(bodyParser.json());
 
-
-
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Objet créé !'
+  });
+});
 app.use('/api/stuff', (req, res, next) => {
   const stuff = [
     {
-      _id: 'oeihfzeoi',
+      _id: '',
       title: 'Mon premier objet',
       description: 'Les infos de mon premier objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-      price: 4900,
-      userId: 'qsomihvqios',
+      imageUrl: '',
+      price: 4,
+      userId: '',
     },
     {
-      _id: 'oeihfzeomoihi',
+      _id: '',
       title: 'Mon deuxième objet',
       description: 'Les infos de mon deuxième objet',
-      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      imageUrl: '',
       price: 2900,
-      userId: 'qsomihvqios',
+      userId: '',
     },
   ];
   res.status(200).json(stuff);
 });
 
+
+const mongoose = require('mongoose');
+
+ mongoose.connect('mongodb+srv://Salima83:<Elienarbonne201814>@cluster0.4jcmm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+  
 module.exports = app;
